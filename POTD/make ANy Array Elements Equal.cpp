927eb0d23d87a4de649be4d8c6f5df4a7044d915
Vasant Phirke <125212752+VBPMP@users.noreply.github.com>
2023-04-01 16:13:15 +0530

    Add files via upload

---
+++ POTD/make ANy Array Elements Equal.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long int minOperations(int N) {
+        // Code here
+        long long int mid=N/2;
+        
+        long long int ans=mid*(mid+1);
+        if(N%2==0)
+        {
+            ans-=(mid);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.minOperations(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
